452b9f93399dc2f88e8a0f1feaacd31050453191
aleksey2101 <>
2021-05-22 22:03:21 +0300

    lab4 layout

--- app/src/main/java/com/example/programm/myapplication_2/Lab4Fragment.java
+++ app/src/main/java/com/example/programm/myapplication_2/Lab4Fragment.java
@@ -1,178 +1,165 @@
 package com.example.programm.myapplication_2;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.io.File;
-import java.io.FileOutputStream;
-
-import android.app.Activity;
-import android.hardware.Camera;
-import android.hardware.Camera.PictureCallback;
-import android.media.CamcorderProfile;
-import android.media.MediaRecorder;
-import android.os.Bundle;
-import android.os.Environment;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+
+import java.util.Objects;
 
 public class Lab4Fragment extends Fragment {
 
-    View rootView;
-    SurfaceView surfaceView;
-    Camera camera;
-    MediaRecorder mediaRecorder;
+    private static final int ID_APP = 7861242;
 
-    File photoFile;
-    File videoFile;
     public static Lab4Fragment newInstance() {
         return new Lab4Fragment();
     }
 
+    final static String TAG="myLogs";
+    private View rootView;
+//    private OnFragmentInteractionListener mListener;
+    private Bundle webViewBundle;
+
+    public Lab4Fragment() {
+        // Required empty public constructor
+    }
+
+
+    boolean isActionBarClosed = false;
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        Log.d(TAG, "setUserVisibleHint is working");
 
-        rootView = inflater.inflate(R.layout.lab4_fragment, container, false);
+        Log.d(TAG, "isVisibleToUser = "+isVisibleToUser);
 
-        File pictures = Environment
-                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        photoFile = new File(pictures, "myphoto.jpg");
-        videoFile = new File(pictures, "myvideo.3gp");
+        if(isVisibleToUser){
+            //скрываем actionbar
+            Objects.requireNonNull(((MainActivity) Objects.requireNonNull(getActivity())).getSupportActionBar()).hide();
+            //скрыто ли уже
+            isActionBarClosed=true;
+        }
+        else if(isActionBarClosed){
+            Objects.requireNonNull(((MainActivity) Objects.requireNonNull(getActivity())).getSupportActionBar()).show();
+            isActionBarClosed=false;
+        }
 
-        surfaceView = (SurfaceView) rootView.findViewById(R.id.surfaceView);
+//        Toast toast = Toast.makeText(rootView.getContext(), "Neither does this", Toast.LENGTH_LONG);
+//        toast.show();
+    }
 
-        Button btnTakePicture = rootView.findViewById(R.id.btnTakePicture);
-        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+
+        /*WebView mWebView = (WebView) rootView.findViewById(R.id.VKWebView);
+        //попробоавть перенести в активити класс всё это (WebView)
+        mWebView.setWebViewClient(new MyWebViewClient());*/
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView= inflater.inflate(R.layout.lab4_fragment, container, false);
+        WebView webView = rootView.findViewById(R.id.VKWebView);
+
+        if (webViewBundle != null)
+        {
+
+            webView.restoreState(webViewBundle);
+        } else {
+            //todo webview пока пустая
+            String htmlText = "<html><body>Percent test: 100% </body></html>";
+
+            webView.loadData(htmlText, "text/html", "en_US");
+        }
+
+        webView.setWebViewClient(new MyWebViewClient());
+        //переопределяет ли он этим метод вызова setWebView
+
+        Button VKBtn = rootView.findViewById(R.id.enterVKBtn);
+        VKBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onClickPicture();
+                setWebView("https://oauth.vk.com/authorize?client_id=7861242&display=page&redirect_uri=https://www.google.com/&scope=friends&response_type=token&v=5.131&state=1337");
             }
         });
 
-        Button btnStartRecord = rootView.findViewById(R.id.btnStartRecord);
-        btnStartRecord.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onClickStartRecord();
-            }
-        });
-
-        Button btnStopRecord = rootView.findViewById(R.id.btnStopRecord);
-        btnStopRecord.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onClickStopRecord();
-            }
-        });
-
-        SurfaceHolder holder = surfaceView.getHolder();
-        holder.addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                try {
-                    camera.setPreviewDisplay(holder);
-                    camera.startPreview();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format,
-                                       int width, int height) {
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-            }
-        });
         return rootView;
     }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OpenFragment.OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        camera = Camera.open();
+    public void onDetach() {
+        super.onDetach();
+//        mListener = null;
+    }
+
+    public void setWebView(String url) {
+        WebView webView = rootView.findViewById(R.id.VKWebView);
+
+        //webView.setBackgroundColor(getResources().getColor(R.color.));
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setDefaultTextEncodingName("utf-8");
+        // устанавливаем Zoom control
+        //webView.getSettings().setBuiltInZoomControls(true);
+
+        //webSettings.setJavaScriptEnabled(true);
+        //скрытие всплывающего окна, но отключение рекламы на сайте
+
+        //webView.loadDataWithBaseURL(null,);
+        webView.loadUrl(url);
     }
 
     @Override
-    public void onPause() {
+    public void onPause()
+    {
         super.onPause();
-        releaseMediaRecorder();
-        if (camera != null)
-            camera.release();
-        camera = null;
+        WebView webView = rootView.findViewById(R.id.VKWebView);
+        webViewBundle = new Bundle();
+        webView.saveState(webViewBundle);
+        //Log.i(TAG+" proky","onPause is working...");
     }
 
-    public void onClickPicture() {
-        camera.takePicture(null, null, new PictureCallback() {
-            @Override
-            public void onPictureTaken(byte[] data, Camera camera) {
-                try {
-                    FileOutputStream fos = new FileOutputStream(photoFile);
-                    fos.write(data);
-                    fos.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+    private static class MyWebViewClient extends WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            //todo в зависимости он выбранной настройки - открывать внутри или в браузере (опционально)
 
-    }
+            //todo определить список ссылок которые надо открывать внутри приложения
+            view.loadUrl(url);
+            return true;
 
-    public void onClickStartRecord() {
-        if (prepareVideoRecorder()) {
-            mediaRecorder.start();
-        } else {
-            releaseMediaRecorder();
-        }
-    }
-
-    public void onClickStopRecord() {
-        if (mediaRecorder != null) {
-            mediaRecorder.stop();
-            releaseMediaRecorder();
-        }
-    }
-
-    private boolean prepareVideoRecorder() {
-
-        camera.unlock();
-
-        mediaRecorder = new MediaRecorder();
-
-        mediaRecorder.setCamera(camera);
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
-        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-        mediaRecorder.setProfile(CamcorderProfile
-                .get(CamcorderProfile.QUALITY_HIGH));
-        mediaRecorder.setOutputFile(videoFile.getAbsolutePath());
-        mediaRecorder.setPreviewDisplay(surfaceView.getHolder().getSurface());
-
-        try {
-            mediaRecorder.prepare();
-        } catch (Exception e) {
-            e.printStackTrace();
-            releaseMediaRecorder();
-            return false;
-        }
-        return true;
-    }
-
-    private void releaseMediaRecorder() {
-        if (mediaRecorder != null) {
-            mediaRecorder.reset();
-            mediaRecorder.release();
-            mediaRecorder = null;
-            camera.lock();
+            /*
+            // все остальные ссылки будут спрашивать какой браузер открывать
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            activity.startActivity(intent);
+            */
         }
     }
 
